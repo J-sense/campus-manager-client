042aea5579020e98245a2a9401e4c846eb477639
J-sense <jishan1873@gmail.com>
2025-01-11 21:45:47 +0600

    decode token

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,16 +1,22 @@
 import { useForm } from "react-hook-form";
 import { useLoginMutation } from "../redux/features/auth/authApi";
+import { useAppDispatch } from "../redux/hooks";
+import { setUser } from "../redux/features/auth/authSlice";
+import { varifyToken } from "../utils/VvarifyToken";
 
 const Login = () => {
+  const dispatch = useAppDispatch();
   const { register, handleSubmit } = useForm();
   const [login, { data, error }] = useLoginMutation();
   console.log(data);
-  const onSubmit = (data) => {
+  const onSubmit = async (data) => {
     const userInfo = {
       id: data.id,
       password: data.password,
     };
-    login(userInfo);
+    const res = await login(userInfo).unwrap();
+    const user = varifyToken(res.data.accessToken);
+    dispatch(setUser({ user: user, token: res.data.accessToken }));
   };
   return (
     <div>
